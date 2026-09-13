f48e39020e56d256f64ea5483d68db247e0bf88c
Naveen <108810648+nv19090@users.noreply.github.com>
2026-09-14 00:14:00 +0530

    Create Experiment6.2.sql

---
+++ Experiment-6/Experiment6.2.sql
@@ -0,0 +1,25 @@
+Create TABLE KRG (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(100),
+    salary NUMERIC(10,2)
+);
+INSERT INTO KRG(emp_id,emp_name,salary) values(1,'A',10000);
+INSERT INTO KRG(emp_id,emp_name,salary) values(2,'B',20000);
+INSERT INTO KRG(emp_id,emp_name,salary) values(3,'C',30000);
+INSERT INTO KRG(emp_id,emp_name,salary) values(4,'D',35000);
+
+create view krg2 AS 
+  select emp_id,emp_name from krg
+  where emp_id=2
+
+
+select * from KRG;
+create materialized view krg3 
+AS 
+  select emp_id,emp_name from krg
+  with no data
+
+select * from krg3
+where emp_id=3
+
+refresh materialized view krg3
